17f96e80d36ad9e2c11e556a898328c5903591a2
Alex Hotsyk <alexhotsyk@gmail.com>
2023-01-19 14:15:27 +0200

    Added methods

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Block } from './components/Block';
 import './index.scss';
 
@@ -6,41 +6,67 @@ function App() {
   const [fromCurrency, setFromCurrency] = useState('UAH')
   const [toCurrency, setToCurrency] = useState('USD')
   const [fromPrice, setFromPrice] = useState(0)
-  const [toPrice, setToPrice] = useState(0)
-  const [rates, setRates] = useState([])
-  let data
+  const [toPrice, setToPrice] = useState(1)
+  const rates = useRef({})
 
+  // Get currency from API
   useEffect(() => {
     fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
     .then(res => res.json())
     .then((json) => {
-      data = json
+      let data = json
       const currency = {
         'UAH': 1,
         'USD': data.find(item => item.cc === 'USD').rate,
         'EUR': data.find(item => item.cc === 'EUR').rate,
         'ILS': data.find(item => item.cc === 'ILS').rate
       }
-      setRates(currency)
+      rates.current = currency
+      onChangeToPrice(1)
     })
     .catch((error) => {
       console.warn(error)
+      alert('Error connection')
     })
   }, [])
 
+  // Change first price method
   const onChangeFromPrice = (value) => {
-    const price = value / rates[toCurrency] 
-    const result = price * rates[fromCurrency]
+    const price = value / rates.current[toCurrency] 
+    const result = price * rates.current[fromCurrency]
     setFromPrice(value)
     setToPrice(result)
   }
 
+  // Change second price method
   const onChangeToPrice = (value) => {
-    const price = value / rates[fromCurrency] 
-    const result = price * rates[toCurrency]
+    const price = value / rates.current[fromCurrency] 
+    const result = price * rates.current[toCurrency]
     setFromPrice(result)
     setToPrice(value)
   }
+
+  // Change first currency method
+  const onChangeFromCurrency = (cur) => {
+    setFromCurrency(cur)
+    onChangeFromPrice(fromPrice)
+  }
+
+  // Change second currency method
+  const onChangeToCurrency = (cur) => {
+    setToCurrency(cur)
+    onChangeToPrice(toPrice)
+  }
+
+  // Listening changes of price
+  useEffect(() => {
+    onChangeFromPrice(fromPrice)
+  }, [fromCurrency])
+  
+  // Listening changes of price
+  useEffect(() => {
+    onChangeToPrice(toPrice)
+  }, [toCurrency])
 
   return (
     <div className="App">
@@ -48,12 +74,13 @@ function App() {
         value={fromPrice} 
         currency={fromCurrency} 
         onChangeValue={onChangeFromPrice}
-        onChangeCurrency={(cur) => console.log(cur)} 
+        onChangeCurrency={onChangeFromCurrency} 
       />
       <Block 
         value={toPrice} 
         currency={toCurrency}  
         onChangeValue={onChangeToPrice}
+        onChangeCurrency={onChangeToCurrency} 
       />
     </div>
   );
